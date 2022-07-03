0e2601cae5c6195f7380902568c15c5537416c97
Noxira <13520054@std.stei.itb.ac.id>
2022-07-03 08:33:30 +0700

    v0.1 of fe

---
+++ fe/kantin-kejujuran/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+// import HelloWorld from "./components/HelloWorld.vue";
+const todos_asc = ([{"content": "Make A Video", "category": "business", "done": false, "createdAt": 1653906191876}])
+</script>
+
+<template>
+	<main class="app">
+		
+		<section class="app-title">
+			<h2 class="title">
+				Kantin Kejujuran
+			</h2>
+		</section>
+
+		<section class="item-list">
+			<div class="list" id="todo-list">
+
+				<div v-for="item in todos_asc" :class="`item ${item.done && 'done'}`">
+					<label>
+						<input type="checkbox" v-model="item.done" />
+						<span :class="`bubble ${
+							item.category == 'business' 
+								? 'business' 
+								: 'personal'
+						}`"></span>
+					</label>
+
+					<div class="item-content">
+						<input type="text" v-model="item.content" />
+					</div>
+
+					<div class="actions">
+						<button class="delete" @click="removeTodo(todo)">Delete</button>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+	</main>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
